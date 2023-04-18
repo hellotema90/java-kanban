5f92e720b788b3c567cf06598bcb1c3cd8663980
hellotema90 <as.masalkin@yandex.ru>
2023-04-18 23:19:50 +0300

    8й спринт2

--- test/HTTPTaskManagerTest.java
+++ test/HTTPTaskManagerTest.java
@@ -1,23 +1,25 @@
-import static org.junit.jupiter.api.Assertions.*;
-
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import http.KVServer;
-import http.KVTaskClient;
-import manager.*;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import http.adapters.InstantAdapter;
+import manager.HTTPTaskManager;
+import manager.Managers;
+import manager.TaskManager;
+import org.junit.jupiter.api.*;
 import tasks.Epic;
 import tasks.Status;
 import tasks.Subtask;
 import tasks.Task;
 
-
-
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.time.Instant;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HTTPTaskManagerTest<T extends TaskManagerTest<HTTPTaskManager>> {
     private KVServer server;
@@ -52,6 +54,22 @@ class HTTPTaskManagerTest<T extends TaskManagerTest<HTTPTaskManager>> {
     }
 
     @Test
+    public void shouldUpdateAndDeleteTasks() {
+        Task task1 = new Task("Задача 1", "Описание задачи 1", Status.NEW, Instant.now(), 0);
+        Task task2 = new Task("Задача 2", "Описание задачи 2", Status.NEW, Instant.now(), 0);
+        manager.createTask(task1);
+        manager.createTask(task2);
+        manager.receiveByIdTask(task1.getId());
+        manager.receiveByIdTask(task2.getId());
+        assertEquals(2, manager.getHistory().size());
+        task1.setId(3);
+        manager.updateTask(task1);
+        assertEquals(task1.getId(), 3);
+        manager.clearByIdTask(2);
+        assertEquals(1, manager.getHistory().size());
+    }
+
+    @Test
     public void shouldLoadEpics() {
         Epic epic1 = new Epic("Эпик 1", "описание Эпика 1", Status.NEW, Instant.now(), 0);
         Epic epic2 = new Epic("Эпик 2", "описание Эпика 2", Status.NEW, Instant.now(), 0);
@@ -61,6 +79,21 @@ class HTTPTaskManagerTest<T extends TaskManagerTest<HTTPTaskManager>> {
         manager.receiveByIdEpic(epic2.getId());
         List<Task> list = manager.getHistory();
         assertEquals(manager.getAllEpic(), list);
+    }
+    @Test
+    public void shouldUpdateAndDeleteEpics() {
+        Epic epic1 = new Epic("Эпик 1", "описание Эпика 1", Status.NEW, Instant.now(), 0);
+        Epic epic2 = new Epic("Эпик 2", "описание Эпика 2", Status.NEW, Instant.now(), 0);
+        manager.createEpic(epic1);
+        manager.createEpic(epic2);
+        manager.receiveByIdEpic(epic1.getId());
+        manager.receiveByIdEpic(epic2.getId());
+        assertEquals(2, manager.getHistory().size());
+        epic1.setId(3);
+        manager.updateStatusEpic(1);
+        assertEquals(epic1.getId(), 3);
+        manager.clearByIdEpic(2);
+        assertEquals(1, manager.getHistory().size());
     }
 
     @Test
@@ -79,4 +112,21 @@ class HTTPTaskManagerTest<T extends TaskManagerTest<HTTPTaskManager>> {
         assertEquals(manager.getAllSubtask(), list);
     }
 
+    @Test
+    public void shouldUpdateAndDeleteSubtasks() {
+        Epic epic1 = new Epic("Эпик 1", "описание Эпика 1", Status.NEW, Instant.now(), 0);
+        manager.createEpic(epic1);
+        Subtask subtask1 = new Subtask("Подзадача 1", "описание Подзадачи 1", Status.NEW, epic1.getId(), Instant.now(), 0);
+        Subtask subtask2 = new Subtask("Подзадача 2", "описание Подзадачи 2", Status.NEW, epic1.getId(), Instant.now(), 0);
+        manager.createSubtask(subtask1);
+        manager.createSubtask(subtask2);
+        manager.receiveByIdSubtask(subtask1.getId());
+        manager.receiveByIdSubtask(subtask2.getId());
+        assertEquals(2, manager.getHistory().size());
+        subtask1.setId(3);
+        manager.updateSubtask(subtask1);
+        assertEquals(subtask1.getId(), 3);
+        manager.clearByIdSubtask(2);
+        assertEquals(1, manager.getHistory().size());
+    }
 }
